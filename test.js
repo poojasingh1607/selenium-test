7f73e293d5d7dbace15918fc624a6a2b6bd6843e
poojasingh1607 <47472205+poojasingh1607@users.noreply.github.com>
2019-02-09 14:02:31 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,30 @@
+var webdriver = require('selenium-webdriver');
+
+var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
+
+driver.get('http://www.google.com');
+
+var By = webdriver.By;
+
+driver.findElement(By.name('q')).sendKeys('wiki');
+driver.findElement(By.name('q')).sendKeys('btnG').click();
+driver.wait(check_title, 1000);
+
+function check_title(){
+	var promise = driver.getTitle().then(function(title){
+		if(title == 'wiki-Google Search')
+		{
+			console.log('success');
+			return true;
+		}
+		else
+		{
+			console.log('fail--'+ title);
+		}
+	});
+	return promise;
+}
+
+driver.findElement(By.name('btnK')).sendKeys('Google Search');
+
+
